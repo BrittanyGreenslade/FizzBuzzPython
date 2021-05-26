7a563cf27a0f35c7b1821ed7cf74cde09847247a
Brittany <bgreensl@ualberta.ca>
2021-05-25 19:46:05 -0600

    finished assignment

---
+++ fizzBuzz.py
@@ -0,0 +1,15 @@
+def fizz_buzz(num):
+    if (num % 5 == 0 and num % 3 == 0):
+        print("FizzBuzz")
+    elif (num % 5) == 0:
+        print("Buzz")
+    elif (num % 3) == 0:
+        print("Fizz")
+    else:
+        print("Not divisible")
+
+
+numbers = [12, 7, 8, 25, 9, 15, 22, 18, 10, 14, 30, 11, 26, 32, 50, 19, 21]
+
+for number in numbers:
+    fizz_buzz(number)
